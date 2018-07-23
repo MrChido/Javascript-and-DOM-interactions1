73f418e660954002a52b8095d49d95d34b12d970
Caleb <savershot685@gmail.com>
2018-07-23 11:59:31 -0500

    fun with colors

---
+++ main.js
@@ -0,0 +1,8 @@
+const   myHeading = document.getElementById('myHeading');
+
+myHeading.addEventListener('click', () =>{
+    myHeading.style.background= 'rgb(199,44,72)';
+    myHeading.style.color='#fbaed2';
+  
+
+});
