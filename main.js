e410724bdba64ada10aceb45b53a23ca0ad3f3e6
Caleb <savershot685@gmail.com>
2018-07-23 12:55:08 -0500

    slight changes, no error.
    It seems to activate 1 of the 2 changes i want the button to make...

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const   myHeading = document.getElementById('myHeading');
 const   Transformation = document.getElementById('Transformation');
-const   feature1= document.getElementById('feature1');
+const   feature1 = document.getElementById('feature1');
+const   choice = document.getElementById('choice');
 
 myHeading.addEventListener('click', () =>{
     myHeading.style.background= 'rgb(199,44,72)';
@@ -9,7 +10,7 @@ myHeading.addEventListener('click', () =>{
 
 Transformation.addEventListener('click', () =>{
     feature1.style.color='rgb(0,105,148)';
-    feature1.style.fontSize: 24;
+   feature1.style.fontSize= 34;
 });
 
 
