4e328de4752a8e0ddd789ea4ccc7ecf898e02370
Caleb <savershot685@gmail.com>
2018-07-23 12:43:15 -0500

    giving more interations, or trying to

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const   myHeading = document.getElementById('myHeading');
+const   Transformation = document.getElementById('Transformation');
+const   feature1= document.getElementById('feature1');
 
 myHeading.addEventListener('click', () =>{
     myHeading.style.background= 'rgb(199,44,72)';
     myHeading.style.color='#fbaed2';
-  
-
 });
+
+Transformation.addEventListener('click', () =>{
+    feature1.style.color='rgb(0,105,148)';
+    feature1.style.fontSize: 24;
+});
+
+
+
